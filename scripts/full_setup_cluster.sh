816f39a77396097b60e643113a08fdd681c003a5
Adam Leskis <leskis@gmail.com>
2024-04-02 14:49:29 +0100

    remove unneeded separate registry

--- scripts/full_setup_cluster.sh
+++ scripts/full_setup_cluster.sh
@@ -6,9 +6,6 @@ fi
 # First, tear down anything currently running
 ./scripts/tear_down.sh
 
-# Then, we set up the registry, since we'll need to push container images to that so our cluster can run them
-./scripts/start_registry.sh
-
 # Next, we'll build all the images
 ./scripts/build_containers.sh
 
