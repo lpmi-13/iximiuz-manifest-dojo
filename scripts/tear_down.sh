d7e38f7ef721642182d87fe3e163f71539ee63fe
Adam Leskis <leskis@gmail.com>
2024-04-02 15:15:23 +0100

    remove tear down of monitoring services

--- scripts/tear_down.sh
+++ scripts/tear_down.sh
@@ -2,19 +2,5 @@ for resource in deploy svc sts secrets configmap roles rolebinding sa networkpol
   kubectl delete $resource -n dojo --all;
 done
 
-# also tear down the monitoring stack
-for resource in svc deploy sa clusterrole clusterrolebinding; do
-  kubectl delete $resource -n kube-system kube-state-metrics;
-done
-
-# get rid of all the resources related to prometheus and grafana
-for resource in configmap deploy ds svc; do
-  kubectl delete $resource -n monitoring --all;
-done
-
-for resource in sa clusterrolebinding; do
-  kubectl delete $resource -n monitoring prometheus-scraper;
-done
-
 # remove the load-generator pods
 kubectl delete deploy -n load --all
