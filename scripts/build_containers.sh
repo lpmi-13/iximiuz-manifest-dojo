2e7e81cba0f92974af894577c49d202400ac6c3d
Adam Leskis <leskis@gmail.com>
2024-06-11 12:24:01 +0100

    try to just pull

--- scripts/build_containers.sh
+++ scripts/build_containers.sh
@@ -1,15 +1,16 @@
-podman build -t flask-webserver -f service-src/webserver/Dockerfile service-src/webserver/
-podman tag flask-webserver registry.iximiuz.com/flask-webserver
+podman pull node:22
+# podman build -t flask-webserver -f service-src/webserver/Dockerfile service-src/webserver/
+# podman tag flask-webserver registry.iximiuz.com/flask-webserver
 # podman push registry.iximiuz.com/flask-webserver
-
-podman build -t logs-processor -f service-src/logs-processor/Dockerfile service-src/logs-processor/
-podman tag logs-processor registry.iximiuz.com/logs-processor
+# 
+# podman build -t logs-processor -f service-src/logs-processor/Dockerfile service-src/logs-processor/
+# podman tag logs-processor registry.iximiuz.com/logs-processor
 # podman push registry.iximiuz.com/logs-processor
-
-podman build -t user-info -f service-src/user-info/Dockerfile service-src/user-info/
-podman tag user-info registry.iximiuz.com/user-info
+# 
+# podman build -t user-info -f service-src/user-info/Dockerfile service-src/user-info/
+# podman tag user-info registry.iximiuz.com/user-info
 # podman push registry.iximiuz.com/user-info
-
-podman build -t load-generator -f service-src/load-generator/Dockerfile service-src/load-generator/
-podman tag load-generator registry.iximiuz.com/load-generator
+# 
+# podman build -t load-generator -f service-src/load-generator/Dockerfile service-src/load-generator/
+# podman tag load-generator registry.iximiuz.com/load-generator
 # podman push registry.iximiuz.com/load-generator
