b1389ebd9504e363801e964fbde1f5a464f1ccb3
Adam Leskis <leskis@gmail.com>
2024-06-11 12:01:58 +0100

    lets do it

--- scripts/full_setup_cluster.sh
+++ scripts/full_setup_cluster.sh
@@ -6,18 +6,18 @@ if ! [ -d "./manifests/application" ]; then
 fi
 
 # First, tear down anything currently running
-./scripts/tear_down.sh
+# ./scripts/tear_down.sh
 
 # Next, we'll build all the images
-./scripts/build_containers.sh
+# ./scripts/build_containers.sh
 
 # Clear out current application manifests and copy the templates back in
-rm -rf manifests/application/*
-cp manifests/templates/* ./manifests/application
+# rm -rf manifests/application/*
+# cp manifests/templates/* ./manifests/application
 
 # set up the manifests with one broken configuration
-python3 ./scripts/configure.py
+# python3 ./scripts/configure.py
 
 # And now we can deploy those containers as deployments with corresponding services
 # alongside the monitoring stack (eg. prometheus/grafana)
-./scripts/full_deploy.sh
+# ./scripts/full_deploy.sh
